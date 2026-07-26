8f8f5b0ad231ad7a686e587f727ce602a468f0b2
reftch <j.developer@hotmail.com>
2026-07-26 08:18:07 +0200

    Disabled trace logger level

--- examples/html/main.rs
+++ examples/html/main.rs
@@ -1,7 +1,7 @@
 use r_server::{logger, response, router::Method, server::Server};
 
 fn main() -> std::io::Result<()> {
-    r_server::logger::set_level(logger::LogLevel::Trace);
+    // r_server::logger::set_level(logger::LogLevel::Trace);
     Server::new("0.0.0.0:8080")?
         .route(Method::GET, "/api/v1/inc/:id", |req, res| {
             if let Some(id) = req.param("id") {
