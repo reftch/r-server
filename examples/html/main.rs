e60f30364564b8fcb0701fb84a8a4ec0d59812a9
reftch <j.developer@hotmail.com>
2026-07-30 19:17:02 +0200

    Added sse handler to the response

--- examples/html/main.rs
+++ examples/html/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 use r_server::{response, router::Method, server::http::Server};
 
 fn main() -> std::io::Result<()> {
@@ -5,10 +7,30 @@ fn main() -> std::io::Result<()> {
     Server::new("127.0.0.1:8082")?
         .route(Method::GET, "/api/v1/users/:id", |req, res| {
             if let Some(id) = req.param("id") {
-                // res.send("Hello".to_string());
                 res.content_type(response::ContentType::JSON)
                     .body(format!("{{\"value\":{}}}", id));
             }
+        })
+        .route(Method::GET, "/stream", |_, res| {
+            // res.enable_sse();
+
+            // let res_ptr = res.try_into()?;
+            let res_for_thread = res;
+            thread::spawn(move || {
+                let mut i = 0;
+                loop {
+                    i += 1;
+                    let message = format!("data: {}\n\n", i);
+                    if let Err(e) = res_for_thread.sse(&message) {
+                        eprintln!("SSE connection closed: {}", e);
+                        break;
+                    }
+                    thread::sleep(Duration::from_secs(1));
+                    if i >= 100 {
+                        break;
+                    }
+                }
+            });
         })
         .assets_path("./examples/html/assets")
         .run()?;
--- src/response/mod.rs
+++ src/response/mod.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::io::{self, Write};
+use std::net::TcpStream;
 
 pub mod builder;
 pub mod content_type;
@@ -7,9 +9,41 @@ pub mod status;
 use crate::info;
 use crate::response::builder::ResponseBuilder;
 use crate::server::connection::ConnectionMetadata;
+use crate::server::https::TlsState;
 
 pub use self::content_type::ContentType;
 pub use self::status::Status;
+
+pub trait SseWriter {
+    fn write_sse(&self, data: &[u8]) -> io::Result<()>;
+}
+
+impl SseWriter for TcpStream {
+    fn write_sse(&self, data: &[u8]) -> io::Result<()> {
+        // self.write(data);
+        let mut stream = self.try_clone()?;
+        // stream = self.write(data);
+        stream.write_all(data)
+    }
+}
+
+impl SseWriter for Option<TlsState> {
+    fn write_sse(&self, data: &[u8]) -> io::Result<()> {
+        match self {
+            Some(TlsState::Connected(stream)) => {
+                let mut stream = stream.get_ref().try_clone()?;
+                stream.write_all(data)
+            }
+
+            Some(TlsState::Handshaking(stream)) => {
+                let mut stream = stream.get_ref().try_clone()?;
+                stream.write_all(data)
+            }
+
+            None => Ok(()),
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct Response<'a, T> {
@@ -64,9 +98,46 @@ impl<'a, T> Response<'a, T> {
         self
     }
 
+    pub fn enable_sse(&mut self) -> &mut Self {
+        self.content_type = ContentType::SSE;
+
+        self.header("Cache-Control", "no-cache");
+        self.header("Connection", "keep-alive");
+        self.header("Content-Type", "text/event-stream");
+
+        self
+    }
+
     pub fn send(&mut self, body: String) -> &mut Self {
         info!("Not implemented, echo: {}", body);
         self
+    }
+}
+
+impl<'a, T> Response<'a, T>
+where
+    T: SseWriter,
+{
+    pub fn sse(&self, data: &str) -> io::Result<()> {
+        let payload = format!("data: {}\n\n", data);
+
+        // 1. Define the headers and status line
+        // 2. Use \r\n for proper HTTP line endings
+        // 3. Ensure there is a double \r\n before the first data chunk begins
+        let response = format!(
+            "HTTP/1.1 200 OK\r\n\
+               Content-Type: text/event-stream\r\n\
+               Cache-Control: no-cache\r\n\
+               Connection: keep-alive\r\n\
+               \r\n\
+               {}\n\n",
+            payload
+        );
+
+        // let res = self.build();
+        // match conn.metadata.stream.write(&res) {}
+        // self.metadata.stream.write_sse(res)
+        self.metadata.stream.write_sse(response.as_bytes())
     }
 }
 
